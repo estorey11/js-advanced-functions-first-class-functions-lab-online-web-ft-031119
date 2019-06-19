9f958f39899f01ebaabe9d510be35ad70927cbbe
Eric Storey <estorey@gmail.com>
2019-06-19 15:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const returnFirstTwoDrivers = function(drivers){return drivers.slice(0, 2)}
 const returnLastTwoDrivers = function(drivers){return drivers.slice(-2)}
 
 const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function 
